b874f402bd98654ef0f98304588c822c70373c36
cookiy <cookiy8511@gmail.com>
2020-03-28 10:14:09 +0800

    增加0328每日一题  单词的压缩编码 python可以用trie前缀树，javascript没有现成的trie，用set hash 词尾来解决

--- All problems/medium/820-minimumLengthEncoding.js
+++ All problems/medium/820-minimumLengthEncoding.js
@@ -27,9 +27,36 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 /**
+ * 题意解读：
+ * 通过信息，S= "time#bell#" 和 indexes = [0, 2, 5]，恢复列表[time,me,bell]
+ * 从0位置开始遍历字符串S，遇到#停止，得到time 
+ * 从2位置开始遍历字符串S，遇到#停止，得到me 
+ * 从5位置开始遍历字符串S，遇到#停止，得到bell
+ * 问题，编码字符串S（which可以恢复列表）最短能有多短？
+ * Trie https://zh.wikipedia.org/zh-hans/Trie
+ * 在计算机科学中，trie，又称前缀树或字典树，是一种有序树，用于保存关联数组，其中的键通常是字符串。与二叉查找树不同，键不是直接保存在节点中，而是由节点在树中的位置决定。
+ */
+/**
+ * 剔除重复词尾的思路，通过哈希表降低查询的复杂度(空间换时间)。
+ * 对 words 中的每个元素的词尾做切片并比对，
+ * 如果词尾出现在 words 中，则删除该元素。
+ * 执行用时 :84 ms, 在所有 JavaScript 提交中击败了100.00%的用户
+ * 内存消耗 :43.2 MB, 在所有 JavaScript 提交中击败了100.00%的用户
  * @param {string[]} words
  * @return {number}
  */
-var minimumLengthEncoding = function(words) {
-
-};
+var minimumLengthEncoding = function (words) {
+  let hashSet = new Set(words)
+  for (let item of hashSet) {
+    for (let i = 1; i < item.length; i++) {
+      // 切片，看看是否词尾在 hashSet 中，切片从1开始，只看每个单词的词尾
+      let target = item.slice(i)
+      console.log(item, i, target)
+      hashSet.has(target) && hashSet.delete(target)
+    }
+  }
+  let result = 0
+  // 根据 hashSet 中剩余元素计算最终长度
+  hashSet.forEach(item => result += item.length + 1)
+  return result
+}
