77b04f690e54dbcc74465fd7d2006510e0b506cd
cookiy <cookiy8511@gmail.com>
2020-05-08 09:33:43 +0800

    增加0508每日一题

---
+++ Daily one question/221-maximalSquare.js
@@ -0,0 +1,39 @@
+/**
+221. 最大正方形
+在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+
+示例:
+
+输入: 
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+输出: 4
+ */
+
+
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function (matrix) {
+    let maxSize = 0;
+    let dp = new Array(matrix.length);
+    for (let i = 0; i < dp.length; i++) {
+      dp[i] = new Array(matrix[i].length).fill(0);
+    }
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] === '1') {
+          if (i == 0 || j === 0) dp[i][j] = 1;
+          else
+            dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+          maxSize = Math.max(maxSize, dp[i][j]);
+        }
+      }
+    }
+    return maxSize ** 2;
+  };
