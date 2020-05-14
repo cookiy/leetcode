bec5842d6d15033a8102c53e0c74c76367f841d5
cookiy <cookiy8511@gmail.com>
2020-05-14 10:56:26 +0800

    增加0514每日一题

--- All problems/easy/136-singleNumber.js
+++ All problems/easy/136-singleNumber.js
@@ -1,4 +1,5 @@
 /**
+136. 只出现一次的数字
 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
 
 说明：
@@ -9,7 +10,7 @@
 
 输入: [2,2,1]
 输出: 1
-示例 2:
+示例 2:
 
 输入: [4,1,2,1,2]
 输出: 4
@@ -17,6 +18,7 @@
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/single-number
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+
 /**
  * @param {number[]} nums
  * @return {number}
@@ -30,4 +32,14 @@ var singleNumber = function(nums) {
        res = res ^ nums[i]
     }
     return res;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let ans = nums[0]
+    for (let i =1; i< nums.length; i++) ans ^= nums[i]
+    return ans
 };
