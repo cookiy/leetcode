f9ebd973c095025c5e0a5d9cdd2f7f17ff0d4728
cookiy <cookiy8511@gmail.com>
2020-05-17 15:18:53 +0800

    增加题目

--- All problems/easy/5-longestPalindrome.js
+++ All problems/easy/5-longestPalindrome.js
@@ -1,4 +1,6 @@
-/**给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+/**
+5. 最长回文子串
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 
 示例 1：
 
@@ -9,14 +11,56 @@
 
 输入: "cbbd"
 输出: "bb"
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/longest-palindromic-substring
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+ /**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let ans = '';
+     let n = s.length;
+     let dp = Array.from(new Array(n), () => new Array().fill(0));
+     for(let i = n-1; i >=0; i--) {
+         for(let j = i; j < n; j++) {
+             dp[i][j] = s[i] === s[j] && ( j - i < 2 || dp[i+1][j-1])
+             if(dp[i][j] && j - i + 1 > ans.length) {
+                 ans = s.substr(i,j - i + 1);
+             }
+         }
+     }
+     return ans;
+ }
+ /**
+ * @param {string} s
+ * @return {string}
+ */
 /**
  * @param {string} s
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
+    if(!s || s.length < 2){
+        return s;
+    }
+    let start = 0,end = 0;
+    let n = s.length;
+    // 中心扩展法
+    let centerExpend = (left,right) => {
+        while(left >= 0 && right < n && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+    for(let i = 0;i < n;i++){
+        let len1 = centerExpend(i,i);
+        let len2 = centerExpend(i,i+1);
+        // 两种组合取最大回文串的长度
+        let maxLen = Math.max(len1,len2);
+        if(maxLen > end - start){
+            // 更新最大回文串的首尾字符索引
+            start = i - ((maxLen - 1) >> 1);
+            end = i + (maxLen >> 1);
+        }
+    }
+    return s.substring(start,end+1);
 };
