4433163975fd0b2ece1bfa5bbfd3d1eee1e499c4
cookiy wang <cookiy8511@gmail.com>
2020-08-07 11:35:55 +0800

    增加每日一题

--- All problems/easy/100-isSameTree.py
+++ All problems/easy/100-isSameTree.py
@@ -41,4 +41,10 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        
+        if p == None and q == None:
+            return True
+        if (p != None and q == None) or (p == None and q != None):
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
